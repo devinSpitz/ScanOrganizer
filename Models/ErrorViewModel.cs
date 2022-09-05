65134199d3c4b1ca9fb74d2f5edc7b7cc4f92d3d
Devin Spitz <devin@spitz.onl>
2022-09-05 15:30:42 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ScanOrganizer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
