4d7b4a40c3f7eb73c8b152c23b9c1cffc122fb70
Devin Spitz <devin@spitz.onl>
2022-09-06 18:56:22 +0200

    forgot to add the migration to the git

---
+++ Migrations/20220906164927_renameField.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScanOrganizer.Migrations
+{
+    public partial class renameField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Active",
+                table: "OcrTags",
+                newName: "IsActive");
+
+            migrationBuilder.RenameColumn(
+                name: "Active",
+                table: "FolderScans",
+                newName: "IsActive");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "OcrTags",
+                newName: "Active");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "FolderScans",
+                newName: "Active");
+        }
+    }
+}
